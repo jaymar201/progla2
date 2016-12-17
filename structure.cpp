b6a897d0a7676e9ad9ec74516bf0de370bd24274
Jaymar dela Cruz <djaymar73@yahoo.com>
2016-12-17 17:13:54 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
 using namespace std;
 
-
 struct Person
 {
-	int toes ;
-	int knees ;
-	int shoulder ; 
-	int head ;
+	int toes;
+	int knees;
+	int shoulder; 
+	int head;	
 };
 
 int main()
 {
-  Person Person;
-  cin>>Person.toes;
-  cin>>Person.knees;
-  cin>>Person.shoulder;
-  cin>>Person.head;
-  
- 	return 0;
-}
 	
+Person P[5];
+
+  for (int i=0;i<5;i++)
+	{
+	
+  cin>>P[i].toes;
+  cin>>P[i].knees;
+  cin>>P[i].shoulder;
+  cin>>P[i].head;
+	}
+  for(int i=0;i<5;i++)
+ 	{
+  cout<<"Person number:"<<i+1;
+  cout<<P[i].toes<<"\n";
+  cout<<P[i].knees<<"\n";
+  cout<<P[i].shoulder<<"\n";
+  cout<<P[i].head<<"\n";
+	 }
+	return 0;
+}
