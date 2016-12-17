76fabeb78ea1e41851cd70d8df4b0bfa3379f943
Jaymar dela Cruz <djaymar73@yahoo.com>
2016-12-17 16:53:58 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -1,11 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Person{
 
-	int toes;
-	int knees;
-	int shoulder;
-	int head;
-
+struct Person
+{
+	int toes ;
+	int knees ;
+	int shoulder ; 
+	int head ;
 };
+
+int main()
+
+{
+  Person Person;
+  cin>>Person.toes;
+  cin>>Person.knees;
+  cin>>Person.shoulder;
+  cin>>Person.head;
+  
+	
+	
+	
+	return 0;
+}
+	
